5144cec81de4930e3991d4e03375ad85a927720c
TSGGS <sagioatscie@gmail.com>
2022-11-17 14:49:17 +0800

    TODO: Login

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ changeTitle('./templates/store-header.php', 'GNE Motorcycle Parts Online Shop');
 <main class="content" id="index-content">
     <span id="gne">GNE Motorcycle Parts</span>
     <div id="index-action">
-        <a id="signup" href="#">Sign Up</a>
-        <a id="login" href="#">Login</a>
+        <a id="view-products-action" href="./products.php">View Products</a>
+        <a id="login-action" href="./login.php">Login</a>
     </div>
 </main>
