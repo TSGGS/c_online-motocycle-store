e5c14c6d52b402557b7761b642970a9b95ec9cfc
TSGGS <sagioatscie@gmail.com>
2022-11-17 10:43:09 +0800

    Add: index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo "Hello World";
+require('./includes/helper.php');
+
+changeTitle('./templates/store-header.php', 'GNE Motorcycle Parts Online Shop');
 ?>
+
+<main class="content" id="index-content">
+    <span id="gne">GNE Motorcycle Parts</span>
+    <div id="index-action">
+        <a id="signup" href="#">Sign Up</a>
+        <a id="login" href="#">Login</a>
+    </div>
+</main>
---
+++ templates/store-header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>%TITLE%</title>
+</head>
+<body>
+    <nav class="box-shadow bg-orange" id="top-nav">
+        <div id="top-nav-elements">
+            <a href="./index.php">Home</a>
+            <a href="./products.php">Products</a>
+            <a href="./services.php">Services</a>
+            <a href="./about.php">About</a>
+            <a href="./faq.php">FAQ</a>
+        </div>
+    </nav>
