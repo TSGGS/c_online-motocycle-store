9b96cdf9e5be3b1bdf7241a7e6fac9f7a0e2d38d
TSGGS <sagioatscie@gmail.com>
2022-11-29 00:00:56 +0800

    Add: faq.php

---
+++ faq.php
@@ -0,0 +1,55 @@
+<?php
+require('./includes/helper.php');
+
+changeTitle('./templates/store-header.php', 'GNE - FAQ');
+?>
+    <main class="content background" id="faq-content">
+        <h1>FREQUENTLY ASKED QUESTIONS</h1>
+        <div id="faq-info">
+            <div class="faq-info-item">
+                <div class="faq-question">
+                    What payment method can I use?
+                    <span>&#10095;</span>
+                </div>
+                <div class="faq-answer">
+                    You can pay cash upon delivery.
+                </div>
+            </div>
+            <div class="faq-info-item">
+                <div class="faq-question">
+                    Do you provide repairs?
+                    <span>&#10095;</span>
+                </div>
+                <div class="faq-answer">
+                    Yes, We're happy to serve you in our physical shop located at Palico Imus.
+                </div>
+            </div>
+            <div class="faq-info-item">
+                <div class="faq-question">
+                    When is your next big sale?
+                    <span>&#10095;</span>
+                </div>
+                <div class="faq-answer">
+                    Just keep posted! Sales are exciting as it is! :)
+                </div>
+            </div>
+            <div class="faq-info-item">
+                <div class="faq-question">
+                    When do you ship?
+                    <span>&#10095;</span>
+                </div>
+                <div class="faq-answer">
+                    Shipping time will be confirmed on the order confirmation document.
+                </div>
+            </div>
+            <div class="faq-info-item">
+                <div class="faq-question">
+                    How to contact customer service?
+                    <span>&#10095;</span>
+                </div>
+                <div class="faq-answer">
+                    If you have question regarding our online store please contact: gnemotorparts@gmail.com
+                </div>
+            </div>
+        </div>
+    </main>
