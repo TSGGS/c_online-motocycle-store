c03db5f57a912a4f1aacd966ec9818a5fb97a8c1
TSGGS <sagioatscie@gmail.com>
2022-11-28 23:56:48 +0800

    Add: services.php

---
+++ services.php
@@ -0,0 +1,22 @@
+<?php
+require('./includes/helper.php');
+
+changeTitle('./templates/store-header.php', 'GNE - Services');
+?>
+    <main class="content background" id="services-content">
+        <h1>SERVICES</h1>
+        <div id="services-info">
+            <div class="services-info-item">
+                <img src="./images/design/service.png">
+                <span>OIL RECYCLING</span>
+            </div>
+            <div class="services-info-item">
+                <img src="./images/design/service1.png">
+                <span>MOTORCYCLE CONDITION AND SAFETY CHECK</span>
+            </div>
+            <div class="services-info-item">
+                <img src="./images/design/service2.png">
+                <span>BATTERY RECYCLING</span>
+            </div>
+        </div>
+    </main>
