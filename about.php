b912bc5bc9691f0cc6f4984e92f928fc6fdb2fd2
TSGGS <sagioatscie@gmail.com>
2022-11-29 00:00:29 +0800

    Add: about.php

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+require('./includes/helper.php');
+
+changeTitle('./templates/store-header.php', 'GNE - About');
+?>
+    <main class="content background" id="about-content">
+        <h1>ABOUT US</h1>
+        <div id="about-info">
+            <img src="./images/design/motorcycle1.png" alt="Motorcycle">
+            <span>GNE Motorcycle Parts Shop was first established back in 2012 as a means of creating the best type of store for vehicle enthusiasts. Our goal has always been to inspire a sense of creativity and entrepreneurship, and be more than your average Motorcycle Parts Store.<br><br>Each of our staff members is an automobile lover with years of experience, who is able to offer you the best advice regarding your repair or restoration. Stop by our store today and talk with our friendly employees to learn more about what we can offer you.</span>
+        </div>
+    </main>
